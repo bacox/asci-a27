527adba46ad44ac0374f7387105fb45de7d83659
bacox <bartcox93@gmail.com>
2024-01-22 16:22:13 +0100

    update

--- src/run.py
+++ src/run.py
@@ -14,6 +14,7 @@ def get_algorithm(name: str) -> Blockchain:
         'echo': EchoAlgorithm,
         'election': RingElection,
         'blockchain': BlockchainNode,
+        'validator': Validator
     }
     if name not in algorithms.keys():
         raise Exception(f'Cannot find select algorithm with name {name}')
