dfc2dab8adc4ea714580d1a57c5bd04c8fec2891
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-01-22 16:07:43 +0100

    Transactions setup

--- src/algorithms/client.py
+++ src/algorithms/client.py
@@ -1,10 +1,11 @@
 from ipv8.community import CommunitySettings
 from ipv8.messaging.payload_dataclass import overwrite_dataclass
 from dataclasses import dataclass
-
+from random import randint
 from ipv8.types import Peer
 
 from da_types import Blockchain, message_wrapper
+
 
 # We are using a custom dataclass implementation.
 dataclass = overwrite_dataclass(dataclass)
@@ -16,10 +17,10 @@ dataclass = overwrite_dataclass(dataclass)
 class Transaction:
     """Transaction message type."""
 
-    hop_counter: int
     sender_id: int
     target_id: int
     amount: int
+    nonce: int = 1
     hash: str = None
 
     def create_hash(self) -> None:
@@ -47,30 +48,47 @@ class Client(Blockchain):
         super().__init__(settings)
         self.history = {}
         self.validators = []
-        # self.clients = []
         self.echo_counter = 0
         self.balance = 0
-        self.add_message_handler(Transaction, self.on_message)
+        self.add_message_handler(Transaction, self.on_transaction)
 
     def on_start(self):
         # start by announcing ourselves to our only known validator
-        print(f"Client {self.node_id}: {self.connections=}, {self.nodes=}")
-        validator_index = list(self.nodes.keys())[0]
-        peer = self.nodes[validator_index]
-
+        self.validators = list(self.nodes.keys())
+        peer = self.nodes[self.validators[0]]
         self.ez_send(peer, Announcement(True))
 
-    def send(self):
-        pass
+        # register a task that sends transactions to other clients at semi-random intervals
+        # (target_id assumes that an even number of validators is defined as the first nodes)
+        self.register_task(
+            "random_tx",
+            self.send_amount,
+            [
+                self.node_id + 1 if self.node_id % 1 == 0 else self.node_id - 1,
+                randint(1, self.balance),
+            ],
+            delay=randint(2, 4),
+            interval=randint(4, 10),
+        )
+
+    def send_amount(self, target_id: int, amount: int):
+        """Send some to a target."""
+        if amount <= self.balance:
+            for validator in self.validators:
+                self.ez_send(
+                    self.nodes[validator], Transaction(self.node_id, target_id, amount)
+                )
 
     @message_wrapper(Transaction)
-    async def on_message(self, peer: Peer, payload: Transaction) -> None:
+    async def on_transaction(self, peer: Peer, payload: Transaction) -> None:
         """Upon reception of a transaction."""
         sender_id = self.node_id_from_peer(peer)
         print(
             f"[Node {self.node_id}] Got a message from node: {sender_id}.\t msg id: {payload.message_id}"
         )
         if payload not in self.history:
+            # add amount to balance and add transaction to history
+
             # broadcast
             payload.hop_counter += 1
             self.history[payload.message_id] = payload
