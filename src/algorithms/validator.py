05f766545f2de2a241e62702a2fa53cff3d9f1a9
bacox <bartcox93@gmail.com>
2024-01-22 16:17:51 +0100

    Update

--- src/algorithms/validator.py
+++ src/algorithms/validator.py
@@ -41,6 +41,13 @@ class Validator(Blockchain):
         for peer in self.nodes.values():
             self.ez_send(peer, Announcement(False))
 
+    
+    def execute(self, transaction: Transaction):
+        pass
+
+    def create_initial_transaction(client: Peer):
+        pass
+
     @message_wrapper(Gossip)
     async def on_gossip(self, peer: Peer, payload: Gossip) -> None:
         sender_id = self.node_id_from_peer(peer)
