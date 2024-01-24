26ffa85bbfc32533c7ce3648dbaa5669eb763d8b
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-01-24 13:23:29 +0100

    Removed time from transactions

--- src/algorithms/client.py
+++ src/algorithms/client.py
@@ -1,5 +1,4 @@
 from random import randint
-from time import time
 from ipv8.community import CommunitySettings
 from ipv8.types import Peer
 
--- src/algorithms/validator.py
+++ src/algorithms/validator.py
@@ -1,5 +1,4 @@
 from typing import List
-from time import time
 
 from ipv8.community import CommunitySettings
 from ipv8.types import Peer
@@ -58,7 +57,7 @@ class Validator(Blockchain):
             if node_id not in self.balances:
                 self.balances[node_id] = starting_balance
             transaction = TransactionBody(
-                self.node_id, node_id, self.balances[node_id], int(time()), 0
+                self.node_id, node_id, self.balances[node_id], 0
             )
             print(f"Creating transaction: {transaction=}")
             print(f"{self.clients=}")
