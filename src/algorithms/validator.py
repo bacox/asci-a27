230243ba79189eb1cb575cc4f1cdc9af7cb3d074
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-01-25 16:01:05 +0100

    Processing of election results

--- src/algorithms/validator.py
+++ src/algorithms/validator.py
@@ -11,7 +11,14 @@ from collections import defaultdict
 from ipv8.messaging.serialization import default_serializer
 from hashlib import sha256
 from da_types import Blockchain, message_wrapper
-from .messages import Announcement, Block, BlockVote, TransactionBody, Gossip, BlockHeader
+from .messages import (
+    Announcement,
+    Block,
+    BlockVote,
+    TransactionBody,
+    Gossip,
+    BlockHeader,
+)
 from threading import RLock
 
 from da_types import Blockchain, message_wrapper
@@ -67,7 +74,7 @@ class Validator(Blockchain):
         self.available_stake = 100
         self.active_block_proposal = False
         self.stake_registration = {}  # dict of validatorID : stake
-        self.result_registration = {}  # dict of winnerID : payload
+        self.result_registration = {}  # dict of validatorID : payload
         self.election_random_seed = None
         self.election_winner_id = None
         self.election_announcement_grace_period = (
@@ -99,7 +106,7 @@ class Validator(Blockchain):
         #     "execute_transactions", self.execute_transactions, delay=4, interval=3
         # )
 
-        self.register_task('act_leader', self.act_leader, delay=5, interval=2)
+        self.register_task("act_leader", self.act_leader, delay=5, interval=2)
         self.register_task(
             "send_buffered_transactions",
             self.send_buffered_transactions,
@@ -123,7 +130,7 @@ class Validator(Blockchain):
     # TODO only execute if we have block finality
     def execute_transactions(self, transactions):
         """Executes a set of transactions if approved"""
-        for transaction in transactions:         
+        for transaction in transactions:
             if transaction.sender_id == -1:
                 self.balances[transaction.target_id] += transaction.amount
                 print(f"Executing special {transaction=}")
@@ -138,7 +145,7 @@ class Validator(Blockchain):
             for node_id, peer in self.clients.items():
                 if node_id == target_id or node_id == sender_id:
                     self.ez_send(peer, transaction)
-            
+
             self.pending_transactions.remove(transaction)
             self.finalized_transactions.append(transaction)
 
@@ -152,14 +159,16 @@ class Validator(Blockchain):
         return True
         if len(self.blocks) == 0:
             return True
-        prev_block_hash = b'0'
+        prev_block_hash = b"0"
         if len(self.blocks) > 0:
-            prev_block_hash = sha256(default_serializer.pack('payload', self.blocks[-1])).digest()
+            prev_block_hash = sha256(
+                default_serializer.pack("payload", self.blocks[-1])
+            ).digest()
         if block.prev_block_hash == prev_block_hash:
             # Check all transactions
             return True
         else:
-            print(f'Invalid: {block.prev_block_hash} == {prev_block_hash}')
+            print(f"Invalid: {block.prev_block_hash} == {prev_block_hash}")
             return False
 
     def act_leader(self):
@@ -172,12 +181,18 @@ class Validator(Blockchain):
 
     def form_block(self):
         # We assume that blocks are ordered.
-        prev_block_hash = b'0'
+        prev_block_hash = b"0"
         if len(self.blocks) > 0:
-            prev_block_hash = sha256(default_serializer.pack('payload', self.blocks[-1])).digest()
+            prev_block_hash = sha256(
+                default_serializer.pack("payload", self.blocks[-1])
+            ).digest()
 
-        block = Block(self.get_block_height() + 1, prev_block_hash, int(time()),
-                      [tx for tx in self.pending_transactions[:block_width]])
+        block = Block(
+            self.get_block_height() + 1,
+            prev_block_hash,
+            int(time()),
+            [tx for tx in self.pending_transactions[:block_width]],
+        )
 
         self.blocks.append(block)
 
@@ -186,14 +201,14 @@ class Validator(Blockchain):
             self.ez_send(peer, block)
 
         # We confirm our own block
-        #@TODO: DO conformation
+        # @TODO: DO conformation
         self.broadcast_block_confirmation(block)
 
     def get_block_height(self):
         if len(self.blocks) > 0:
             return self.blocks[-1].block_height
         return 0
-    
+
     def broadcast_block_confirmation(self, block):
         block_package = default_serializer.pack("payload", block)
         block_hash = sha256(block_package).digest()
@@ -201,8 +216,6 @@ class Validator(Blockchain):
         self.block_votes[block_hash].add(self.node_id)
         for peer in self.validators.values():
             self.ez_send(peer, block_vote)
-        
-
 
     def send_buffered_transactions(self):
         """Function to broadcast the buffered transactions on the network."""
@@ -268,20 +281,21 @@ class Validator(Blockchain):
         elif payload.election_round > self.election_round:
             self.election_round = payload.election_round
 
-        # send our own participation if not done yet
-        if self.election_phase == "none":
-            self.election_announce(payload.origin_id)
-
-        # save the received stakes
-        if payload.sender_id not in self.stake_registration:
-            self.stake_registration[payload.sender_id] = payload.stake
-
         # if the message came from an unseen validator, add it to the known validators
         if (
             payload.sender_id not in self.validators
             and payload.sender_id != self.node_id
         ):
             self.validators[payload.sender_id] = peer
+
+        # send our own participation if not done yet
+        if self.election_phase == "none":
+            self.election_announce(payload.origin_id)
+
+        # save the received stakes and broadcast
+        if payload.sender_id not in self.stake_registration:
+            self.stake_registration[payload.sender_id] = payload.stake
+            self.broadcast(payload, peer, validators=True, clients=False)
 
         # if we have received the minimum expected announcements, start a grace period
         if len(self.stake_registration) - 1 >= ceil(
@@ -301,7 +315,9 @@ class Validator(Blockchain):
     def election_announce_winner(self):
         """Calculates and broadcasts the election winner."""
         assert self.election_phase == "announce_grace"
-        self.cancel_pending_task("election_announce_participation_grace_period")
+        self.cancel_pending_task(
+            "election_announce_participation_grace_period"
+        )  # cancel to avoid double execution
         self.election_phase = "elect"
         # print(
         #     f" [V{self.node_id}] Election {self.election_round} phase: {self.election_phase}"
@@ -332,22 +348,48 @@ class Validator(Blockchain):
     @message_wrapper(AnnounceConcensusWinner)
     async def on_election_result(self, peer: Peer, payload: AnnounceConcensusWinner):
         """When an election winner is received, store it for verification."""
-        # TODO
-        # verify the incoming
+        print(f" [V{self.node_id}] received election result from {payload.sender_id}:")
 
-        # if it is not valid, cancel the grace and start a new election round
-        if self.election_phase == "elect_grace":
-            self.cancel_pending_task("election_announce_winner_grace_period")
-        self.election_round += 1
-        self.start_election()
+        # check whether we're able to receive participations
+        if self.election_phase not in ("elect", "elect_grace"):
+            print(
+                f"[V{self.node_id}] received election participation from {payload.sender_id}, but is in phase {self.election_phase}, ignoring"
+            )
+            return
+
+        # check whether this is a valid election round
+        if payload.election_round < self.election_round:
+            print(
+                f"Ignoring received old election announcement (round {payload.election_round=}<{self.election_round=})"
+            )
+            return
+        elif payload.election_round > self.election_round:
+            # self.election_round = payload.election_round
+            raise ValueError("TF is going on here")
+
+        # if the message came from an unseen validator, add it to the known validators
+        if (
+            payload.sender_id not in self.validators
+            and payload.sender_id != self.node_id
+        ):
+            self.validators[payload.sender_id] = peer
+
+        # send our own result if not done yet
+        if self.election_winner_id is None:
+            self.election_announce_winner()
+
+        # save the received results and broadcast
+        if payload.sender_id not in self.result_registration:
+            self.result_registration[payload.sender_id] = payload
+            self.broadcast(payload, peer, validators=True, clients=False)
 
         # if we have received the minimum expected announcements, start a grace period
-        if len(self.stake_registration) >= ceil(
+        if len(self.result_registration) >= ceil(
             len(self.validators) * factor_non_byzantine
         ):
             if self.election_phase == "elect_grace":
                 self.cancel_pending_task("election_announce_winner_grace_period")
-            # after the grace period, announce the winner
+            # after the grace period, ratify the results
             self.election_phase = "elect_grace"
             self.register_task(
                 "election_announce_winner_grace_period",
@@ -357,8 +399,12 @@ class Validator(Blockchain):
 
     def election_ratify(self):
         """If no contradictory results have been received, ratify the election outcome."""
-        assert self.election_phase == "elect_grace"
-        self.cancel_pending_task("election_announce_winner_grace_period")
+        assert (
+            self.election_phase == "elect_grace" and self.election_winner_id is not None
+        )
+        self.cancel_pending_task(
+            "election_announce_winner_grace_period"
+        )  # cancel to avoid double execution
         self.election_phase = "ratify"
 
         # for each result received, check if it confirms our findings
@@ -366,6 +412,8 @@ class Validator(Blockchain):
         # if we have received less than f contradictory results, the leader is known
 
         # if more than f contradictory results are received, a new election must be started
+        self.election_round += 1
+        self.start_election()
 
         # prepare the variables for a next election
         self.election_phase = "none"
@@ -375,11 +423,13 @@ class Validator(Blockchain):
         self.election_round += 1
 
     def finalize_block(self, block: Block):
-        print('Finalizing block')
+        print("Finalizing block")
         self.execute_transactions(block.transactions)
-        
+
         # self.check_transactions(block.transactions)
-        print(f'[Node {self.node_id}] Finalized block {block.block_height} with {len(block.transactions)} transactions')
+        print(
+            f"[Node {self.node_id}] Finalized block {block.block_height} with {len(block.transactions)} transactions"
+        )
         self.active_block_proposal = False
 
     @message_wrapper(Block)
@@ -394,9 +444,13 @@ class Validator(Blockchain):
                 # TODO: there might be soft forks.
                 # @TODO: Call block confirmation
                 self.broadcast_block_confirmation(payload)
-            print(f'[Node {self.node_id}] Received block {payload.block_height} ({len(self.blocks)})')
+            print(
+                f"[Node {self.node_id}] Received block {payload.block_height} ({len(self.blocks)})"
+            )
         else:
-            print(f'[Node {self.node_id}] Received invalid block {payload.block_height}')
+            print(
+                f"[Node {self.node_id}] Received invalid block {payload.block_height}"
+            )
 
     @message_wrapper(Gossip)
     async def on_gossip(self, peer: Peer, payload: Gossip) -> None:
@@ -485,13 +539,17 @@ class Validator(Blockchain):
 
             if self.is_new_transaction(payload):
                 self.buffered_transactions.append(payload)
-    
+
     @message_wrapper(BlockVote)
     async def on_block_vote(self, peer: Peer, payload: BlockVote) -> None:
         # Find corresponding block
-        block = [block for block in self.blocks if block.block_height == payload.block_height]
+        block = [
+            block for block in self.blocks if block.block_height == payload.block_height
+        ]
         if len(block) == 0:
-            print(f'[Node {self.node_id}] Received invalid vote for block {payload.block_height} 1')
+            print(
+                f"[Node {self.node_id}] Received invalid vote for block {payload.block_height} 1"
+            )
             return
         block = block[0]
 
@@ -499,15 +557,20 @@ class Validator(Blockchain):
         block_package = default_serializer.pack("payload", block)
         block_hash = sha256(block_package).digest()
         if block_hash != payload.block_hash:
-            print(f'[Node {self.node_id}] Received invalid vote for block {payload.block_height} 2')
+            print(
+                f"[Node {self.node_id}] Received invalid vote for block {payload.block_height} 2"
+            )
             return
 
         # Record vote
         self.block_votes[payload.block_hash].add(self.node_id_from_peer(peer))
-        print(f'[Node {self.node_id}] Received vote for block {payload.block_height}')
+        print(f"[Node {self.node_id}] Received vote for block {payload.block_height}")
 
         # Check for majority votes (two thirds).
-        if len(self.block_votes[payload.block_hash]) >= 2 * (len(self.validators) + 1) / 3:
+        if (
+            len(self.block_votes[payload.block_hash])
+            >= 2 * (len(self.validators) + 1) / 3
+        ):
             self.finalize_block(block)
             self.block_votes.pop(payload.block_hash)
 
